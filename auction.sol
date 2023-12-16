468a9eaaa8b68b60f2432e4f3971937dec2bf4db
Abdul Rafay <abdulrafay715@gmail.com>
2023-12-16 11:35:32 +0500

    added changes

--- auction.sol
+++ auction.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Auction {
     address public owner;
     uint256 public auctionEndTime;
+    uint256 public auctionStartTime
     uint256 public highestBindingBid;
     uint256 public minBidIncrement;
     address public highestBidder;
@@ -13,30 +14,36 @@ contract Auction {
 
     constructor(uint256 durationMinutes, uint256 _minBidIncrement) {
         owner = msg.sender;
+        auctionStartTime=block.timestamp;
         auctionEndTime = block.timestamp + (durationMinutes * 1 minutes);
         minBidIncrement = _minBidIncrement;
     }
 
     function placeBid() external payable {
-        require(block.timestamp <= auctionEndTime, "Auction has ended");
-        require(msg.value >= highestBindingBid + minBidIncrement, "Bid must be at least minBidIncrement higher");
+        require(block.timestamp <= auctionEndTime, "Auction has ended");//make sure auction is going on
+        require(msg.value >= highestBindingBid + minBidIncrement, "Bid must be at least minBidIncrement higher");//make sure bid amount is higher than before
 
-        if (highestBidder != address(0)) {
-            bids[highestBidder] += highestBindingBid;
+       if (bids[msg.sender] > 0) {
+            bids[msg.sender] += msg.value;
         } else {
-            bidders.push(msg.sender);
+            bidders.push(msg.sender); // Add new bidder to the array
+            bids[msg.sender] = msg.value; // Initialize bid amount for the new bidder
         }
 
         highestBindingBid = msg.value;
         highestBidder = msg.sender;
-        bids[msg.sender] += msg.value;
     }
 
     function finalizeAuction() external {
         require(msg.sender == owner, "Only the owner can finalize the auction");
-        require(block.timestamp >= auctionEndTime, "Auction has not ended yet");
+        require(bidders.length>=3, "At least 3 bidders are required to finalize auction");
+        require(block.timestamp >= auctionStartTime + 2 minutes, "Auction can only be finalized at least 2 minutes after start time");
+        uint256 ownerProfit = (highestBindingBid * 10) / 100; // calculate 10% for the owner
+        uint256 remainingAmount = highestBindingBid - ownerProfit;
 
-        payable(owner).transfer(highestBindingBid);
+        payable(owner).transfer(ownerProfit); //send 10% to owner
+        highestBindingBid = remainingAmount; 
+        
     }
 
     function cancelAuction() external {
